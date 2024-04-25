56305f83fdbdd15a1567c618572f14c82e065c1a
Nontaphat Pongpis <momo5101@hotmail.com>
2024-04-25 18:10:47 +0700

    Add Logo

--- resources/js/script2024.js
+++ resources/js/script2024.js
@@ -39,6 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "การปรับแก้ระบบที่ติด Bug ให้กลับไปทำงานได้ตามปกติเปรียบเสมือน ปัญหาสุดท้ายที่ The Last Station ที่ได้ทำการแก้ไขจุดบกพร่อง เพื่อที่จะให้ผลงานพร้อมที่จะแสดงผลงานต่าง ๆ ให้ผู้อื่นได้เห็น ซึ่งพวกเราจะเป็น Debugger ที่เป็นเครื่องมือที่ใช้หา Bug (ปัญหาต่าง ๆ)",
       majorThai: "วิชาเอกคอมพิวเตอร์เพื่อการสื่อสาร",
       majorEng: "Computer for Communication",
+      logoSrc: "resources/images/2024/logoMajor/Commu.png",
     },
     ticketCinema: {
       major: "KEEP ROLLING THESIS",
@@ -46,6 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut sem lobortis, venenatis nulla ac, cursus lacus. Suspendisse vitae ullamcorper dui. Maecenas aliquet vulputate turpis, non porttitor mauris molestie sit amet.",
       majorThai: "สาขาภาพยนตร์และสื่อดิจิทัล",
       majorEng: "Cinema and Digital Media",
+      logoSrc: "resources/images/2024/logoMajor/Cinema.png",
     },
     ticketCyber: {
       major: "Chill Monument",
@@ -53,6 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut sem lobortis, venenatis nulla ac, cursus lacus. Suspendisse vitae ullamcorper dui. Maecenas aliquet vulputate turpis, non porttitor mauris molestie sit amet.",
       majorThai: "วิชาเอกการจัดการธุรกิจไซเบอร์",
       majorEng: "Cyber Business Management",
+      logoSrc: "resources/images/2024/logoMajor/Cyber.png",
     },
     ticketHealth: {
       major: "SHOC(K)CO FACTORY",
@@ -60,6 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut sem lobortis, venenatis nulla ac, cursus lacus. Suspendisse vitae ullamcorper dui. Maecenas aliquet vulputate turpis, non porttitor mauris molestie sit amet.",
       majorThai: "วิชาเอกการสื่อสารเพื่อสุขภาพ",
       majorEng: "Health Communication",
+      logoSrc: "resources/images/2024/logoMajor/Health.png",
     },
     ticketInno: {
       major: "INNOVERSE Platform",
@@ -67,6 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut sem lobortis, venenatis nulla ac, cursus lacus. Suspendisse vitae ullamcorper dui. Maecenas aliquet vulputate turpis, non porttitor mauris molestie sit amet.",
       majorThai: "วิชาเอกการสื่อสารเพื่อการจัดการนวัตกรรม",
       majorEng: "Innovation Management Communication",
+      logoSrc: "resources/images/2024/logoMajor/Inno.png",
     },
     ticketMulti: {
       major: "End of One - Third",
@@ -74,6 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "พาร์ทในช่วงชีวิตแต่ละคนคงไม่พ้น ชีวิตการเรียน การทำงาน และ แก่ตายไป ในเมื่อเด็กปี 4 ที่กำลังจะจบการศึกษาในเร็ววันนี้ เพื่อจะออกไปทำงานในโลกที่กว้างกว่าเดิม ซึ่งนี่อาจเป็น Station สุดท้ายของใครหลายคนในชีวิตการเรียน และ Thesis นี้จะเป็นบทสรุป ของ 1 ส่วน 3",
       majorThai: "วิชาเอกการออกแบบสื่อปฎิสัมพันธ์และมัลติมีเดีย",
       majorEng: "Interactive and Multimedia Design",
+      logoSrc: "resources/images/2024/logoMajor/Multi.png",
     },
     ticketTourism: {
       major: "THEAW KROB ROD",
@@ -81,22 +87,25 @@ document.addEventListener("DOMContentLoaded", function () {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut sem lobortis, venenatis nulla ac, cursus lacus. Suspendisse vitae ullamcorper dui. Maecenas aliquet vulputate turpis, non porttitor mauris molestie sit amet.",
       majorThai: "วิชาเอกการสื่อสารเพื่อการท่องเที่ยว",
       majorEng: "Tourism Communication",
+      logoSrc: "resources/images/2024/logoMajor/Tour.png",
     },
   };
 
   // Function to change the description
   function changeDescription(ticketId) {
     const descriptionDiv = document.querySelector(".contain-description");
+
     descriptionDiv.querySelector(".head-ticket-description").textContent =
       ticketDescriptions[ticketId].major;
-
-    // For the detailed description
     descriptionDiv.querySelector(".ticket-description").textContent =
       ticketDescriptions[ticketId].description;
     descriptionDiv.querySelector(".major-thai").textContent =
       ticketDescriptions[ticketId].majorThai;
     descriptionDiv.querySelector(".major-eng").textContent =
       ticketDescriptions[ticketId].majorEng;
+
+    const majorLogoImg = document.querySelector(".img-major");
+    majorLogoImg.src = ticketDescriptions[ticketId].logoSrc;
   }
 
   Object.keys(ticketDescriptions).forEach(function (ticketId) {
